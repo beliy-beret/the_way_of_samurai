929ae717a294434e1ee9aea6d13327e0248ebd14
AlucarD <skoriy1290@gmail.com>
2022-01-30 16:24:57 +0300

    add some style for pagination

--- src/components/usersPage/UsersPage.jsx
+++ src/components/usersPage/UsersPage.jsx
@@ -2,19 +2,37 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { fetchUserList } from '../../redux/usersReducer';
+import ReactPaginate from 'react-paginate';
 import User from './User';
 import style from './usersStyle.module.css';
 
 export default function UsersPage() {
   const dispatch = useDispatch();
-  const { currentPage, userList } = useSelector((state) => state.users);
+  const { userList, pageSize, totalUsers, currentPage } = useSelector(
+    (state) => state.users
+  );
+  const totalPages = Math.ceil(totalUsers / pageSize);
   useEffect(() => {
     dispatch(fetchUserList(currentPage));
   }, [currentPage, dispatch]);
   console.log(userList);
   return (
     <section className={style.usersPage}>
-      <h2 className={style.title}>All users</h2>
+      <div className={style.header}>
+        <h2 className={style.title}>All users</h2>
+        <ReactPaginate
+          pageCount={totalPages}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={5}
+          previousLabel={'prev'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          initialPage={currentPage}
+          containerClassName={style.pagination}
+          activeClassName={style.selected}
+          pageClassName={style.page}
+        />
+      </div>
       {userList?.map((user) => (
         <User key={user.id} user={user} />
       ))}
